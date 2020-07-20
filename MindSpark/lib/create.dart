ca6d4bd1829e4079a658000c1d8c979fe10ac879
Zareeb_Chowdhury <38160697+ZareebC@users.noreply.github.com>
2020-07-20 14:06:26 -0400

    Added Animations and Buttons to create page

--- MindSpark/lib/create.dart
+++ MindSpark/lib/create.dart
@@ -1,36 +1,111 @@
+import 'package:animated_widgets/animated_widgets.dart';
+import 'package:animated_widgets/widgets/opacity_animated.dart';
 import 'package:flutter/material.dart';
+import 'package:nice_button/NiceButton.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() => runApp(new Create());
 
-class Create extends StatelessWidget{
+class Create extends StatefulWidget {
+  @override
+  _CreateState createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  var firstColor = Color(0xff5b86e5), secondColor = Color(0xff36d1dc);
+  bool enabled = true;
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: Text("Create")
-        ),
-        drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-                DrawerHeader(
-                    child: Text("Header"),
-                  ),
-                ListTile(
-                    title: Text("Home"),
-                  )
-              ],
-            ),
-        ),
+    return SafeArea(
+      child: new Scaffold(
         body: new Container(
-          child: new Center(
-            child: new Column(
-              children: <Widget>[
-                new Text("Create")
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+              colors: [
+                Colors.black38,
+                Colors.black54,
               ],
+              begin: const FractionalOffset(0.5, 0.0),
+              end: const FractionalOffset(0.0, 0.5),
+              stops: [0.0,1.0],
+              tileMode: TileMode.clamp
             ),
           ),
+          child: new Column(
+            children: <Widget>[
+              new SizedBox(height: 15,),
+              new TyperAnimatedTextKit(
+                text: ["", "Create"],
+                textStyle: TextStyle(fontSize: 30.0),
+                isRepeatingAnimation: false,
+              ),
+              new SizedBox(height: 10,),
+              new Align(
+                alignment: Alignment(-.9, -.7),
+                child: new GestureDetector(
+                  child: Icon(Icons.arrow_drop_down_circle),
+                  onTap: () => Navigator.pop(context),
+                ) 
+              ),
+              new SizedBox(height:150),
+              TranslationAnimatedWidget.tween(
+                enabled: enabled,
+                translationDisabled: Offset(200, 0),
+                translationEnabled: Offset(0, 0),
+                child:
+                  OpacityAnimatedWidget.tween(
+                  enabled: enabled,
+                  opacityDisabled: 0,
+                  opacityEnabled: 1,
+                  child: new NiceButton(
+                    radius: 30,
+                    padding: const EdgeInsets.all(15),
+                    text: "Register",
+                    icon: Icons.account_box,
+                    gradientColors: [secondColor, firstColor],
+                  ),
+                ),
+              ),
+              new SizedBox(height: 50),
+              TranslationAnimatedWidget.tween(
+                enabled: enabled,
+                translationDisabled: Offset(-200, 0),
+                translationEnabled: Offset(0, 0),
+                child:
+                  OpacityAnimatedWidget.tween(
+                  enabled: enabled,
+                  opacityDisabled: 0,
+                  opacityEnabled: 1,
+                  child: new NiceButton(
+                    radius: 30,
+                    padding: const EdgeInsets.all(15),
+                    text: "Register",
+                    icon: Icons.account_box,
+                    gradientColors: [Colors.pink, Colors.purple],
+                  ),
+                ),
+              ),
+              new SizedBox(height: 50),
+              TranslationAnimatedWidget.tween(
+                enabled: enabled,
+                translationDisabled: Offset(200, 0),
+                translationEnabled: Offset(0, 0),
+                child:
+                  OpacityAnimatedWidget.tween(
+                  enabled: enabled,
+                  opacityDisabled: 0,
+                  opacityEnabled: 1,
+                  child: new NiceButton(
+                    radius: 30,
+                    padding: const EdgeInsets.all(15),
+                    text: "Register",
+                    icon: Icons.account_box,
+                    gradientColors: [Colors.amber, Colors.orange],
+                  ),
+                ),
+              ),
+            ],
+          )
         ),
       ),
     );
